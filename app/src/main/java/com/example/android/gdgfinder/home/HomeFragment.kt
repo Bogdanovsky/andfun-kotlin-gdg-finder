8ae79a5fab5af0009f202d392e868d914c16b036
Vasily Bogdanov <30338221+Bogdanovsky@users.noreply.github.com>
2022-09-13 14:53:37 +0300

    step 4

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,15 +24,20 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // TODO (06) Create a binding to the home_fragment layout and tell the binding
-        // about the viewModel.
+        val binding = HomeFragmentBinding.inflate(inflater)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.viewModel = viewModel
 
         val view = inflater.inflate(R.layout.home_fragment, container, false)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        // TODO (07)  Register an observer on navigateToSearch, and have it navigate
-        // to gdgListFragment if shouldNavigate is true.
-
-        return view
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer { navigate ->
+            if (navigate) {
+                val navController = binding.root.findNavController()
+                navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+            }
+        })
+        return binding.root
     }
 }
